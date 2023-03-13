3e59d4a5dfad720485ff8df23518586af9791e84
Saravanan S <sec20cb018@sairamtap.edu.in>
2023-03-13 11:53:30 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     const [navs,setNav] = useState(false)
     const handleNav = () => {setNav(!navs)}
     return(
-        <div className="flex fixed w-[100%] top-0 justify-between items-center h-20 mx-auto px-4 text-white back bg-cover">
+        <div className="flex fixed w-[100%] top-0 justify-between items-center h-20 mx-auto px-4 text-white bg-slate-900 bg-cover">
             <Link to="/" className="w-full text-xl font-cinzel font-bold text-white" onClick={()=>{window.scrollTo(0, 0)}}>
             <img src={Navlogo} alt="festx" className=' w-[100px] h-[30px]'/>
             </Link>
@@ -25,11 +25,11 @@ const Navbar = () => {
             <div className={navs ? "fixed left-0 top-0 w-[40%] h-full border-r border-r-gray-900 back ease-in-out duration-700 lg:hidden" : "fixed left-[-100%]"}>
             <h1 className="w-full text-3xl font-cinzel font-bold text-white m-4" onClick={()=>{window.scrollTo(0, 0)}} ><img src={Navlogo} alt="festx" className=' w-[100px] h-[30px]'/></h1>
                 <nav className="uppercase text-xl p-5 flex flex-col" ref={navRef}>
-                    <Link to="/" className=" p-4 border-b border-b-gray-300" onClick={()=>{window.scrollTo(0, 0)}} >Home</Link>
-                    <Link to="/events" className=" p-4 border-b border-b-gray-300" onClick={()=>{window.scrollTo(0, 0)}} >Events</Link>
+                    <Link to="/" className=" p-4 border-b border-b-gray-300" onClick={()=>{window.scrollTo(0, 0); setNav(false);}} >Home</Link>
+                    <Link to="/events" className=" p-4 border-b border-b-gray-300" onClick={()=>{window.scrollTo(0, 0); setNav(false);}} >Events</Link>
                     {/* <Link to="/speakers" className="p-4 border-b border-b-gray-300">Speakers</Link> */}
                     {/* <Link to="/sponsors" className="p-4 border-b border-b-gray-300">Sponsors</Link> */}
-                    <Link to="/contact" className=" p-4" onClick={()=>{window.scrollTo(0, 0)}} >Contact</Link>
+                    <Link to="/contact" className=" p-4" onClick={()=>{window.scrollTo(0, 0); setNav(false);}} >Contact</Link>
                 </nav>
             </div>
         </div>
